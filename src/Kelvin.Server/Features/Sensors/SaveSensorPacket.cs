887de20e70aaa20ed9474b044b2fc344d492bad5
Michael DeBerry <mdeberry@vitreousoft.com>
2026-08-19 00:10:00 -0400

    invalidated cache for new sensor

--- src/Kelvin.Server/Features/Sensors/SaveSensorPacket.cs
+++ src/Kelvin.Server/Features/Sensors/SaveSensorPacket.cs
@@ -3,20 +3,24 @@ using Kelvin.Server.Channels;
 using Kelvin.Server.Data;
 using Kelvin.Server.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Kelvin.Server.Features.Sensors;
 
 public record SaveSensorPacketRequest(SensorPacket SensorPacket) : IRequest;
 
-public class SaveSensorPacketHandler(KelvinContext context, ISensorPacketChannel sensorPacketChannel) : IHandler<SaveSensorPacketRequest>
+public class SaveSensorPacketHandler(KelvinContext context, ISensorPacketChannel sensorPacketChannel, IMemoryCache cache)
+  : IHandler<SaveSensorPacketRequest>
 {
   public async Task<Result> HandleAsync(SaveSensorPacketRequest request, CancellationToken ct = default)
   {
     var sensor = await context.Sensors.FirstOrDefaultAsync(s => s.MacAddress == request.SensorPacket.MacAddress, ct);
+    var clearCache = false;
     if (sensor is null)
     {
       sensor = new Sensor { MacAddress = request.SensorPacket.MacAddress, Enabled = true };
       context.Sensors.Add(sensor);
+      clearCache = true;
     }
 
     // if it was deleted, but starts sending packets again restore it, but leave it disabled.
@@ -29,6 +33,11 @@ public class SaveSensorPacketHandler(KelvinContext context, ISensorPacketChannel
     request.SensorPacket.SensorId = sensor.Id;
     context.SensorPackets.Add(request.SensorPacket);
     await context.SaveChangesAsync(ct);
+
+    if (clearCache)
+    {
+      cache.Remove(SensorsCache.Key);
+    }
 
     // Always save the packet if one comes in so the latest sensor state is retained,
     // but if it's not enabled don't send it to the channel for processing, because we don't want data from disabled sensors to be processed.
